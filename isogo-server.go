482a595d5e65b609520cad8685c382ba477eed3e
Jason Blatt <jlblatt+github@gmail.com>
2018-03-09 18:13:21 -0500

    Init

---
+++ isogo-server.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	fmt.Printf("Isogo Server 0.1\n")
+	fmt.Printf("%s\n", time.Now())
+	fmt.Printf("\"Video games are bad for you? That's what they said about rock n' roll.\" -- Shigeru Miyamoto \n")
+}
+
+// map generator should be a package
